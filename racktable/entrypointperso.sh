2acae99ca7fd4901375899fa54a79071b6ce45ee
julien <julien.jordan@gmail.com>
2018-07-05 11:31:46 +0200

    Update entrypointperso.sh

--- racktable/entrypointperso.sh
+++ racktable/entrypointperso.sh
@@ -1,1 +1,32 @@
+#!/bin/sh -e
 
+: "${DBNAME:=racktables}"
+: "${DBHOST:=mariadb}"
+: "${DBUSER:=racktables}"
+
+if [ ! -e /opt/racktables/wwwroot/inc/secret.php ]; then
+    cat > /opt/racktables/wwwroot/inc/secret.php <<EOF
+<?php
+\$pdo_dsn = 'mysql:host=${DBHOST};dbname=${DBNAME}';
+\$db_username = '${DBUSER}';
+\$db_password = '${DBPASS}';
+\$user_auth_src = 'database';
+\$require_local_account = TRUE;
+# See https://wiki.racktables.org/index.php/RackTablesAdminGuide
+function terminal_settings (\$cell, \$params)
+{
+	\$params[0]['protocol'] = 'telnet';
+       	\$params[0]['username'] = 'root';
+	\$params[0]['password'] = '${TELNETPW}';
+       	\$params[0]['connect_timeout'] = 5;
+}
+?>
+EOF
+fi
+
+chmod 0400 /opt/racktables/wwwroot/inc/secret.php
+chown nobody:nogroup /opt/racktables/wwwroot/inc/secret.php
+
+echo 'To initialize the db, first go to /?module=installer&step=5'
+
+exec "$@"
